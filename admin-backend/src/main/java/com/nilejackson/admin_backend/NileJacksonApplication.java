1560c7400c59fbb29b5bcdb87e50617df9023fbe
Nile Jackson <nilejack@udel.edu>
2025-04-03 19:39:46 -0400

    Add Spring Boot backend structure

---
+++ admin-backend/src/main/java/com/nilejackson/admin_backend/NileJacksonApplication.java
@@ -0,0 +1,13 @@
+package com.nilejackson.admin_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NileJacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NileJacksonApplication.class, args);
+	}
+
+}
